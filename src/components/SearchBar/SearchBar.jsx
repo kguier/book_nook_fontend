48b5327f9040e9144cd2e5b1a9a9f51e098db019
Kira <knguier12@gmail.com>
2024-01-11 21:24:11 -0500

    Search function completed

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,10 +1,37 @@
 import React, { useState } from "react";
+import "./SearchBar.css";
 
 function SearchBar({ placeholder, data }) {
+  const [input, setInput] = useState("");
+  const fetchData = (value) => {
+    fetch("https://www.googleapis.com/books/v1/volumes?q={SEARCH QUERY HERE}")
+      .then((response) => response.json())
+      .then((json) => {
+        const results = json.items.filter((book) => {
+          return (
+            book &&
+            book.volumeInfo &&
+            book.volumeInfo.title &&
+            book.volumeInfo.title.toLowerCase().includes(value)
+          );
+        });
+        console.log(results);
+      });
+  };
+
+  const handleChange = (value) => {
+    setInput(value);
+    fetchData(value);
+  };
   return (
-    <div ckassName="search">
+    <div className="search">
       <div className="searchInputs">
-        <input type="text" placeholder={placeholder} />
+        <input
+          type="text"
+          placeholder="Enter Book Details Here"
+          value={input}
+          onChange={(e) => handleChange(e.target.value)}
+        />
       </div>
     </div>
   );
